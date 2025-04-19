b734cd6b5c46a35ae160f31985fd57ab5e558640
Ayush <ayushjadaun6@gmail.com>
2025-04-20 02:30:42 +0530

    fix route

--- backend/routes/userSubscription.routes.js
+++ backend/routes/userSubscription.routes.js
@@ -5,7 +5,7 @@ import {
   getSubscriptionById,
   updateSubscription,
   deleteSubscription,
-} from "../controllers/userSubscription.controller.js";
+} from "../controllers/usersubscription.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
