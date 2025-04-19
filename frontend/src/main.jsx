8b1eb25c522ba0fddd3b5a3bccb11730bd79ff6f
Ayush <ayushjadaun6@gmail.com>
2025-04-20 03:42:45 +0530

    redux persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,21 +2,17 @@ import React, { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import App from "./App.jsx";
-import store from "./store/store.js";
+import store, { persistor } from "./store/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { checkAuthStatus } from "./store/slices/authSlice";
-
 
 const AppWrapper = () => {
   useEffect(() => {
-
     store.dispatch(checkAuthStatus());
-
-
     const interval = setInterval(() => {
       store.dispatch(checkAuthStatus());
     }, 15 * 60 * 1000);
-
     return () => clearInterval(interval);
   }, []);
 
@@ -26,7 +22,9 @@ const AppWrapper = () => {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <AppWrapper />
+      <PersistGate loading={null} persistor={persistor}>
+        <AppWrapper />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
